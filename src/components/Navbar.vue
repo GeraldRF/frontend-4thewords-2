63a98fbb0bc16dab13e5ab62640db704bc02df7d
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-02 01:54:29 -0600

    Se agrego heroicons y un menu responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,129 @@
 <template>
-  <nav>
-    <div>
-      <router-link to="/">Libro Virtual</router-link>
+  <nav
+    class="
+      p-2
+      flex
+      w-full
+      h-15
+      items-center
+      justify-between
+      md:justify-start
+      bg-blue-700
+    "
+  >
+    <div class="p-2 md:mr-5 pr-6 text-xl text-white md:border-r-2">
+      <router-link to="/" class="flex items-center">
+        <BookOpenIcon class="w-10"></BookOpenIcon>
+        LibroVirtual</router-link
+      >
     </div>
-    <ul>
-      <li>
-        <router-link to="/" class="p-2 text-red-600 bg-gray-300 rounded">Home</router-link>
+
+    <button class="flex md:hidden" ref="openMenu">
+      <MenuIcon class="w-10 text-white"></MenuIcon>
+    </button>
+
+    <ul
+      class="
+        hidden
+        w-80
+        h-screen
+        fixed
+        top-0
+        right-0
+        pt-20
+        md:pt-0
+        shadow-md
+        md:shadow-none md:relative md:mt-0 md:flex
+        flex-col
+        md:flex-row md:h-auto
+        gap-5
+        md:gap-3
+        items-center
+        text-center
+        bg-blue-700
+      "
+      ref="menu"
+    >
+      <li class="md:hidden">
+        <button
+          class="flex md:hidden absolute right-3 top-3 z-10"
+          ref="closeMenu"
+        >
+          <XIcon class="w-10 text-white"></XIcon>
+        </button>
       </li>
-      <li>
-        <router-link to="/agregar">Agrega un suceso</router-link> |
+      <li class="w-full md:w-auto">
+        <router-link
+          to="/"
+          class="
+            p-2
+            text-xl
+            md:text-lg
+            w-full
+            text-white
+            hover:text-blue-700 hover:bg-white
+            rounded
+          "
+          >Home</router-link
+        >
       </li>
-      <li>
-        <router-link to="/top-10-sucesos-mas-populares">Top 10</router-link>
+      <li class="w-full md:w-auto">
+        <router-link
+          to="/agregar"
+          class="
+            p-2
+            text-xl
+            md:text-lg
+            text-white
+            hover:text-blue-700 hover:bg-white
+            rounded
+          "
+          >Agrega un suceso</router-link
+        >
+      </li>
+      <li class="w-full md:w-auto">
+        <router-link
+          to="/top-10-sucesos-mas-populares"
+          class="
+            p-2
+            text-xl
+            md:text-lg
+            text-white
+            hover:text-blue-700 hover:bg-white
+            rounded
+          "
+          >Top 10</router-link
+        >
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
+import { BookOpenIcon, MenuIcon, XIcon } from "@heroicons/vue/solid";
+
 export default {
   name: "Navbar",
+  components: { BookOpenIcon, MenuIcon, XIcon },
+  mounted() {
+    let _this = this;
+    this.$refs.openMenu.addEventListener("click", function () {
+      let menu = _this.$refs.menu;
+      let closeMenu = _this.$refs.closeMenu;
+      menu.classList.add("flex");
+      menu.classList.remove("hidden");
+      menu.classList.add("showMenu");
+      closeMenu.addEventListener("click", function () {
+        menu.classList.remove("showMenu");
+        menu.classList.add("closeMenu");
+        setTimeout(function () {
+          menu.classList.add("hidden");
+          menu.classList.remove("flex");
+          menu.classList.remove("closeMenu");
+        }, 450);
+      });
+    });
+  },
 };
 </script>
 
