32fe52afa71b86b7281aa0acdda468f5e1a68d6e
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-05 01:58:56 -0600

    Se agrego una lupa y se pusieron rutas hacia mapa y chrons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,73 +7,57 @@
       h-15
       items-center
       justify-between
-      md:justify-start
+      lg:justify-start
       bg-blue-700
+      shadow-lg
+      z-10
     "
   >
-    <div class="p-2 md:mr-5 pr-6 text-3xl text-white md:border-r-2">
+    <div class="p-2 md:mr-5 pr-6 text-2xl text-white lg:border-r-2">
       <router-link to="/" class="flex items-center">
         <BookOpenIcon class="w-10"></BookOpenIcon>
         LibroVirtual</router-link
       >
     </div>
 
-    <button class="flex md:hidden" ref="openMenu">
-      <MenuIcon class="w-10 text-white"></MenuIcon>
-    </button>
-
     <ul
       class="
         hidden
         w-72
-        md:w-full
+        lg:w-full
         h-screen
         fixed
         top-0
         right-0
         pt-20
-        md:pt-0
+        lg:pt-0
         shadow-md
-        md:shadow-none md:relative md:mt-0 md:flex
+        lg:shadow-none lg:relative lg:mt-0 lg:flex
         flex-col
-        md:flex-row md:h-auto
+        lg:flex-row lg:h-auto
         gap-5
-        md:gap-3
+        lg:gap-3
         items-center
         text-center
         bg-blue-700
+        z-10
       "
       ref="menu"
     >
-      <li class="md:hidden">
+      <li class="lg:hidden">
         <button
-          class="flex md:hidden absolute right-3 top-3 z-10"
+          class="flex lg:hidden absolute right-3 top-3 z-10 text-white hover:bg-white hover:text-blue-700 rounded"
           ref="closeMenu"
         >
-          <XIcon class="w-10 text-white"></XIcon>
+          <XIcon class="w-10"></XIcon>
         </button>
-      </li>
-      <li class="w-full md:w-auto">
-        <router-link
-          to="/"
-          class="
-            p-2
-            text-2xl
-            w-full
-            text-white
-            hover:text-blue-700 hover:bg-white
-            rounded
-          "
-          >Inicio</router-link
-        >
       </li>
       <li class="w-full md:w-auto">
         <router-link
           to="/agregar"
           class="
             p-2
-            text-2xl
-            text-white
+            text-lg text-white
             hover:text-blue-700 hover:bg-white
             rounded
           "
@@ -82,11 +66,10 @@
       </li>
       <li class="w-full md:w-auto">
         <router-link
-          to="/top-10-sucesos-mas-populares"
+          to="/top-10"
           class="
             p-2
-            text-2xl
-            text-white
+            text-lg text-white
             hover:text-blue-700 hover:bg-white
             rounded
           "
@@ -95,27 +78,54 @@
       </li>
       <li class="w-full md:w-auto">
         <router-link
-          to="/sucesos-en-orden-cronologico"
+          to="/orden-cronologico"
           class="
             p-2
-            text-2xl
-            text-white
+            text-lg text-white
             hover:text-blue-700 hover:bg-white
             rounded
           "
           >Orden cronologico</router-link
         >
       </li>
+      <li class="w-full md:w-auto">
+        <router-link
+          to="/mapa"
+          class="
+            p-2
+            text-lg text-white
+            hover:text-blue-700 hover:bg-white
+            rounded
+          "
+          >Mapa de los sucesos</router-link
+        >
+      </li>
     </ul>
+
+    <div class="flex lg:mr-3">
+      <button
+        class="text-white hover:bg-white hover:text-blue-700 rounded"
+      >
+        <SearchIcon class="w-9"></SearchIcon>
+      </button>
+      <button class="flex lg:hidden text-white hover:bg-white hover:text-blue-700 rounded" ref="openMenu">
+        <MenuIcon class="w-10"></MenuIcon>
+      </button>
+    </div>
   </nav>
 </template>
 
 <script>
-import { BookOpenIcon, MenuIcon, XIcon } from "@heroicons/vue/solid";
+import {
+  BookOpenIcon,
+  MenuIcon,
+  XIcon,
+  SearchIcon,
+} from "@heroicons/vue/solid";
 
 export default {
   name: "Navbar",
-  components: { BookOpenIcon, MenuIcon, XIcon },
+  components: { BookOpenIcon, MenuIcon, XIcon, SearchIcon },
   mounted() {
     let _this = this;
     this.$refs.openMenu.addEventListener("click", function () {
