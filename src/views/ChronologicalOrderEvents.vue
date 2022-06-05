02d01ef63975abbbc1b9dd4ea681c80bbbd698ad
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-05 01:58:13 -0600

    Se agrego la vista EventsMap y ChronologicalOrderEvents

---
+++ src/views/ChronologicalOrderEvents.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Chron</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/EventsMap.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Map purpose="showAllData" :historicalEvents="this.historicalEvents" height="90vh"></Map>
+  </div>
+</template>
+
+<script>
+import Map from "@/components/Map.vue";
+
+export default {
+  name: "EventsMap",
+  components: {
+   Map
+  },
+  data() {
+    return {
+      historicalEvents: [],
+    };
+  },
+  mounted() {
+    this.axios
+      .get("http://localhost:8000/api/historical-events")
+      .then((response) => {
+        this.historicalEvents = response.data.data;
+      });
+  },
+};
+</script>
+
+<style>
+</style>
