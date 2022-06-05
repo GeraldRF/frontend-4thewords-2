e9891cceb286273da878f2a0ee9371f89ecc308c
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-05 01:56:11 -0600

    Se implemento el HistoricalEventList e HistoricalEvent

---
+++ src/components/HistoricalEvent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full shadow">
+    <h2>
+      {{ historicalEvent.name }}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'HistoricalEvent',
+    props: {
+        historicalEvent: Object
+    }
+};
+</script>
---
+++ src/components/HistoricalEventList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="py-3 w-full flex flex-col gap-2">
+    <HistoricalEvent
+      v-for="hEvent in historicalEvents"
+      :key="hEvent.id" :historicalEvent="hEvent"
+    ></HistoricalEvent>
+  </div>
+</template>
+
+<script>
+import HistoricalEvent from "./HistoricalEvent.vue";
+export default {
+  name: "HistoricalEventList",
+  components: { HistoricalEvent },
+  props: {
+    historicalEvents: Array,
+  },
+};
+</script>
