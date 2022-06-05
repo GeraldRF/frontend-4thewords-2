da2c581150540ee04980eaf62a53407c17a4ad23
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-05 06:23:21 -0600

    Se editor el navbar para que sea responsive en lg y se agrego un boton de busqueda

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,118 +1,140 @@
 <template>
-  <nav
-    class="
-      p-2
-      flex
-      w-full
-      h-15
-      items-center
-      justify-between
-      lg:justify-start
-      bg-blue-700
-      shadow-lg
-      z-10
-    "
-  >
-    <div class="p-2 md:mr-5 pr-6 text-2xl text-white lg:border-r-2">
-      <router-link to="/" class="flex items-center">
-        <BookOpenIcon class="w-10"></BookOpenIcon>
-        LibroVirtual</router-link
-      >
-    </div>
-
-    <ul
+  <div class="w-full h-16">
+    <div class="h-16 w-full"></div>
+    <nav
       class="
-        hidden
-        w-72
-        lg:w-full
-        h-screen
+        p-2
+        flex
+        w-full
+        h-16
+        items-center
+        justify-between
+        lg:justify-start
+        bg-blue-700
+        shadow-lg
+        z-10
         fixed
         top-0
-        right-0
-        pt-20
-        lg:pt-0
-        shadow-md
-        lg:shadow-none lg:relative lg:mt-0 lg:flex
-        flex-col
-        lg:flex-row lg:h-auto
-        gap-5
-        lg:gap-3
-        items-center
-        text-center
-        bg-blue-700
-        z-10
       "
-      ref="menu"
     >
-      <li class="lg:hidden">
-        <button
-          class="flex lg:hidden absolute right-3 top-3 z-10 text-white hover:bg-white hover:text-blue-700 rounded"
-          ref="closeMenu"
+      <div class="p-2 md:mr-5 pr-6 text-2xl text-white lg:border-r-2">
+        <router-link to="/" class="flex items-center">
+          <BookOpenIcon class="w-10"></BookOpenIcon>
+          LibroVirtual</router-link
         >
-          <XIcon class="w-10"></XIcon>
-        </button>
-      </li>
-      <li class="w-full md:w-auto">
-        <router-link
-          to="/agregar"
-          class="
-            p-2
-            text-lg text-white
-            hover:text-blue-700 hover:bg-white
-            rounded
-          "
-          >Agrega un suceso</router-link
-        >
-      </li>
-      <li class="w-full md:w-auto">
-        <router-link
-          to="/top-10"
-          class="
-            p-2
-            text-lg text-white
-            hover:text-blue-700 hover:bg-white
-            rounded
-          "
-          >Top 10</router-link
-        >
-      </li>
-      <li class="w-full md:w-auto">
-        <router-link
-          to="/orden-cronologico"
-          class="
-            p-2
-            text-lg text-white
-            hover:text-blue-700 hover:bg-white
-            rounded
-          "
-          >Orden cronologico</router-link
-        >
-      </li>
-      <li class="w-full md:w-auto">
-        <router-link
-          to="/mapa"
-          class="
-            p-2
-            text-lg text-white
-            hover:text-blue-700 hover:bg-white
-            rounded
-          "
-          >Mapa de los sucesos</router-link
-        >
-      </li>
-    </ul>
+      </div>
 
-    <div class="flex lg:mr-3">
-      <button
-        class="text-white hover:bg-white hover:text-blue-700 rounded"
+      <ul
+        class="
+          hidden
+          w-72
+          lg:w-full
+          h-screen
+          fixed
+          top-0
+          right-0
+          pt-20
+          lg:pt-0
+          shadow-md
+          lg:shadow-none lg:relative lg:mt-0 lg:flex
+          flex-col
+          lg:flex-row lg:h-auto
+          gap-5
+          lg:gap-3
+          items-center
+          text-center
+          bg-blue-700
+          z-10
+        "
+        ref="menu"
       >
-        <SearchIcon class="w-9"></SearchIcon>
-      </button>
-      <button class="flex lg:hidden text-white hover:bg-white hover:text-blue-700 rounded" ref="openMenu">
-        <MenuIcon class="w-10"></MenuIcon>
-      </button>
-    </div>
-  </nav>
+        <li class="lg:hidden">
+          <button
+            class="
+              flex
+              lg:hidden
+              absolute
+              right-3
+              top-3
+              z-10
+              text-white
+              hover:bg-white hover:text-blue-700
+              rounded
+            "
+            ref="closeMenu"
+          >
+            <XIcon class="w-10"></XIcon>
+          </button>
+        </li>
+        <li class="w-full md:w-auto">
+          <router-link
+            to="/agregar"
+            class="
+              p-2
+              text-lg text-white
+              hover:text-blue-700 hover:bg-white
+              rounded
+            "
+            >Agrega un suceso</router-link
+          >
+        </li>
+        <li class="w-full md:w-auto">
+          <router-link
+            to="/top-10"
+            class="
+              p-2
+              text-lg text-white
+              hover:text-blue-700 hover:bg-white
+              rounded
+            "
+            >Top 10</router-link
+          >
+        </li>
+        <li class="w-full md:w-auto">
+          <router-link
+            to="/orden-cronologico"
+            class="
+              p-2
+              text-lg text-white
+              hover:text-blue-700 hover:bg-white
+              rounded
+            "
+            >Orden cronologico</router-link
+          >
+        </li>
+        <li class="w-full md:w-auto">
+          <router-link
+            to="/mapa"
+            class="
+              p-2
+              text-lg text-white
+              hover:text-blue-700 hover:bg-white
+              rounded
+            "
+            >Mapa de los sucesos</router-link
+          >
+        </li>
+      </ul>
+
+      <div class="flex lg:mr-3">
+        <button class="text-white hover:bg-white hover:text-blue-700 rounded">
+          <SearchIcon class="w-9"></SearchIcon>
+        </button>
+        <button
+          class="
+            flex
+            lg:hidden
+            text-white
+            hover:bg-white hover:text-blue-700
+            rounded
+          "
+          ref="openMenu"
+        >
+          <MenuIcon class="w-10"></MenuIcon>
+        </button>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
