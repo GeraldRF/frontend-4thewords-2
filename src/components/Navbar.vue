95a37ce2cba407b1dcb9f21ad6d07c39fd222712
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-05 19:46:35 -0600

    Se implemento el buscar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -117,9 +117,55 @@
       </ul>
 
       <div class="flex lg:mr-3">
-        <button class="text-white hover:bg-white hover:text-blue-700 rounded">
+        <button
+          @click="toggleSearchBar"
+          class="text-white hover:bg-white hover:text-blue-700 rounded"
+        >
           <SearchIcon class="w-9"></SearchIcon>
         </button>
+        <div
+          ref="searchBar"
+          class="p-3 w-full hidden justify-end fixed top-14 right-0 z-10"
+        >
+          <form
+            @submit.prevent="searchEvent"
+            class="
+              flex
+              w-full
+              sm:w-6/12
+              justify-end
+              gap-2
+              bg-white
+              border-2
+              rounded
+              shadow-lg
+            "
+          >
+            <input
+              ref="inputBar"
+              type="text"
+              class="p-2 w-11/12 outline-none text-xl"
+            />
+            <select ref="by">
+              <option value="name">Nombre</option>
+              <option value="country">Pais</option>
+            </select>
+            <button
+              type="submit"
+              class="
+                m-1
+                p-2
+                bg-blue-800
+                hover:bg-blue-400
+                text-white
+                rounded-full
+              "
+            >
+              <SearchIcon class="w-5"></SearchIcon>
+            </button>
+          </form>
+        </div>
+
         <button
           class="
             flex
@@ -166,6 +212,24 @@ export default {
         }, 450);
       });
     });
+  },
+  methods: {
+    toggleSearchBar() {
+      let bar = this.$refs.searchBar;
+      bar.classList.toggle("hidden");
+      bar.classList.toggle("flex");
+      this.$refs.inputBar.focus();
+    },
+    searchEvent() {
+      if (this.$refs.inputBar.value != "") {
+        this.$router.push({
+          name: "Home",
+          query: { q: this.$refs.inputBar.value, by: this.$refs.by.value },
+        });
+        this.toggleSearchBar();
+        this.$refs.inputBar.value = "";
+      }
+    },
   },
 };
 </script>
